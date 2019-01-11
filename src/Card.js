7a2b5703a31bbf86520f8a0645ed85ff972dddec
Robert MAC <robert.baddeley@gmail.com>
2019-01-11 07:44:48 -1000

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -19,11 +19,25 @@ const Tile = styled.section`
     }
 `
 
+const Button = styled.a`
+    background: lightgreen;
+    border-radius: 2px;
+
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+
+    :hover {
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25),
+                    0 10px 10px rgba(0,0,0,0.22);
+`
+
 const Card = ({name, gender, dob}) => {
     return (
         <Tile>
             <h2>{name}</h2>
-            {gender} || {dob}
+            {gender} || Birthyear: {dob}
+
+            <Button/>
         </Tile>
     )
 }
