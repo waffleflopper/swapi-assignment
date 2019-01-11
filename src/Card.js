96c1a5a8bf5b50343b237faee2d7e290648101b9
Robert Baddeley <robertbaddeley@gmail.com>
2019-01-11 06:22:32 -1000

    added some stylesss

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,20 @@ import styled from 'styled-components';
 
 const Tile = styled.section`
     display: inline-block;
-    margin: 1.5em;
-    padding: .5em;
+    background: #fff;
+    border-radius: 2px;
+    height: 300px;
+    margin: 1rem;
+    position: relative;
+    width: 300px
     
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+
+    :hover {
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25),
+                    0 10px 10px rgba(0,0,0,0.22);
+    }
 `
 
 const Card = ({name, gender, dob}) => {
